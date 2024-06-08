3669177f69eed9a93d26be4f4053a8512e0ef517
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-07 21:54:54 -0300

    Atividade Concluída

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿decimal distancia, calculo, tempo;
+ 
+Console.WriteLine("---ATIVIDADE RAIO E TROVÃO---\n");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.Write("Digite a quantos segundos você ouviu o trovão:");
+Console.ResetColor();
+ 
+ 
+tempo = Convert.ToInt32(Console.ReadLine());
+calculo = Convert.ToDecimal(340.29);
+ 
+distancia = tempo * calculo;
+ 
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine($"A distância do raio é de {distancia}KM ");
+Console.ResetColor();
